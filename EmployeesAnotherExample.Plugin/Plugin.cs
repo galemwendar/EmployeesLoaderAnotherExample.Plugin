7c7fa0d861e66d9df50a6e37278a5e0aee5dce10
123 <123@DESKTOP-JHFTG9T>
2023-02-17 01:20:47 +0300

    Добавьте файлы проекта.

---
+++ EmployeesAnotherExample.Plugin/Plugin.cs
@@ -0,0 +1,63 @@
+﻿using PhoneApp.Domain.Attributes;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EmployeesLoaderAnotherExample.Plugin
+{
+    [Author(Name = "Andrew Petrov")]
+    public class Plugin : IPluggable
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
+        {
+            logger.Info("Loading employees");
+            var employeesList = new List<EmployeesDTO>();
+            try
+            {
+                var userList = GetEmplyeesFromDummyJson();
+                employeesList = userList.Select(user => new EmployeesDTO
+                {
+                    Name = $"{user.FirstName} {user.LastName}",
+                }).ToList();
+
+                foreach (var user in userList)
+                {
+                    var employee = employeesList.First(e => e.Name == $"{user.FirstName} {user.LastName}");
+                    employee.AddPhone(user.Phone);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                logger.Error(ex.Message);
+                logger.Error(ex.StackTrace);
+            }
+            logger.Info($"Loaded {employeesList.Count()} employees");
+            return employeesList.Cast<DataTransferObject>();
+
+        }
+
+        public List<UserData> GetEmplyeesFromDummyJson()
+        {
+            string url = "https://dummyjson.com/users";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream resStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(resStream);
+            string json = reader.ReadToEnd();
+            //deserialize json
+            var employeeJson = Newtonsoft.Json.JsonConvert.DeserializeObject<UsersJson>(json);
+            return employeeJson.Users;
+
+        }
+    }
+}
---
+++ EmployeesAnotherExample.Plugin/UsersDTO.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesLoaderAnotherExample.Plugin
+{
+    public class UsersJson
+    {
+        public List<UserData> Users { get; set; }
+        public int Total { get; set; }
+        public int Skip { get; set; }
+        public int Limit { get; set; }
+    }
+
+    public class UserData
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MaidenName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Image { get; set; }
+        public string BloodGroup { get; set; }
+        public decimal Height { get; set; }
+        public decimal Weight { get; set; }
+        public string EyeColor { get; set; }
+        public Hair Hair { get; set; }
+        public string Domain { get; set; }
+        public string Ip { get; set; }
+        public AddressDTO Address { get; set; }
+        public string MacAddress { get; set; }
+        public string University { get; set; }
+        public Bank Bank { get; set; }
+        public Company Company { get; set; }
+        public string Ein { get; set; }
+        public string Ssn { get; set; }
+        public string UserAgent { get; set; }
+    }
+
+    public class Hair
+    {
+        public string Color { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class AddressDTO
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+        public Coordinates Coordinates { get; set; }
+        public string PostalCode { get; set; }
+        public string State { get; set; }
+    }
+
+    public class Coordinates
+    {
+        public decimal Lat { get; set; }
+        public decimal Lng { get; set; }
+    }
+
+    public class Bank
+    {
+        public string CardExpire { get; set; }
+        public string CardNumber { get; set; }
+        public string CardType { get; set; }
+        public string Currency { get; set; }
+        public string Iban { get; set; }
+    }
+
+    public class Company
+    {
+        public AddressDTO Address { get; set; }
+        public string Department { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+    }
+}
